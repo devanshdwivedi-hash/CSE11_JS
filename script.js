8a35b4c39c6f28b96b250ac3eafa103e1b443a4b
devansh_dwivedi <devanshd310@gmail.com>
2026-03-06 22:00:00 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+//console.log("Hello World!");
+//document.write("Hello World!");
+
+//var count = 0
+//document.write("Starting Loop" + "<br/>");
+//while(count<10){
+//    document.write("Curernt Count:" + count + "<br/>");
+//    count++;
+//}
+//document.write("Loop Stopped!");
+
+//var grade="A";
+//document.write("Entering switch block <br/>");
+//switch (grade){
+//    case 'A':document.write("good job!");
+//    break;
+//    case 'B':document.write("keep it up!");
+//    break;
+//    case 'C':document.write("try harder!");
+//    break;
+//    case 'D':document.write("need improvement!");
+//    break;
+//    default: document.write("you failed");
+//}
+
